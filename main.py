8604e9bef5572e7544cd79a7cfae08505c8f9df5
trick491 <143121688+trick491@users.noreply.github.com>
2025-03-23 19:07:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random, time
 import tabulate
-
+import sys
+sys.setrecursionlimit(10000)
 def ssort(L):
     ### selection sort
     if (len(L) == 1):
@@ -10,11 +11,17 @@ def ssort(L):
         print('selecting minimum %s' % L[m])       
         L[0], L[m] = L[m], L[0]
         print('recursively sorting L=%s\n' % L[1:])
-        return [L[0]] + selection_sort(L[1:])
+        return [L[0]] + ssort(L[1:])
         
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
+    if len(a) <= 1:
+        return a
+    else:
+        pivot = pivot_fn(a)
+        left = [x for x in a if x < pivot]
+        middle = [x for x in a if x == pivot]
+        right = [x for x in a if x > pivot]
+        return qsort(left, pivot_fn) + middle + qsort(right, pivot_fn)
     
 def time_search(sort_fn, mylist):
     """
@@ -50,20 +57,19 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
-        ])
+        random.shuffle(mylist)
+
+        qsort_fixed_pivot = time_search(lambda lst: qsort(lst, lambda x: x[0]), mylist)
+        qsort_random_pivot = time_search(lambda lst: qsort(lst, lambda x: random.choice(x)), mylist)
+        tim_sort = time_search(lambda lst: sorted(lst), mylist) 
+
+        ssort_time = time_search(ssort, mylist) if int(size) <= 2000 else None
+
+        result.append([size, qsort_fixed_pivot, qsort_random_pivot, tim_sort, ssort_time])
     return result
     ###
 
